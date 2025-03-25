a50e3088df31183c1f10c5087f446146d6caad23
Ricketson Palacio <i_ricketson.palacio@stratpoint.com>
2025-03-25 13:40:03 +0800

    feat(data): Added movie service for fetching movies of different genres

---
+++ lib/features/movie/data/services/movie_service.dart
@@ -0,0 +1,123 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_intro_bootcamp_project/core/api_dio_client.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_details_model.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_model.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_reviews_model.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/trailers_model.dart';
+
+enum MovieGenre {
+  action(28),
+  adventure(12),
+  animation(16),
+  comedy(35),
+  crime(80),
+  documentary(99),
+  drama(18),
+  family(10751),
+  fantasy(14),
+  history(36),
+  horror(27),
+  music(10402),
+  mystery(9648),
+  romance(10749),
+  thriller(53),
+  war(10752);
+
+  const MovieGenre(this.id);
+  final int id;
+}
+
+class MovieService {
+  MovieService(this.apiClient);
+  final ApiDioClient apiClient;
+
+  final String? apiKey = dotenv.env['TMDB_API_KEY'];
+
+  Future<List<MediaContentModel>> fetchMoviesByGenre(MovieGenre genre) async {
+    try {
+      final Response<dynamic> response = await apiClient.get('discover/movie?api_key=$apiKey&with_genres=${genre.id}');
+      if (response.statusCode == 200 && response.data != null) {
+        List<dynamic> moviesJson = response.data!['results'];
+        return moviesJson.map((dynamic item) => MediaContentModel.fromJson(item)).toList();
+      } else {
+        throw Exception('Failed to fetch movies for genre: ${genre.name}');
+      }
+    } catch (e) {
+      throw Exception('Error fetching movies for genre: ${genre.name}, Error: $e');
+    }
+  }
+
+  Future<TrailersModel?> fetchTrailers(int movieId) async {
+    try {
+      final Response<dynamic> response = await apiClient.get('movie/$movieId/videos?api_key=$apiKey');
+      if (response.statusCode == 200 && response.data != null) {
+        List<dynamic> videos = response.data!['results'];
+        if (videos.isNotEmpty) {
+          return TrailersModel.fromJson(videos.first);
+        } else {
+          return null; // Return null if no videos found
+        }
+      } else {
+        throw Exception('Failed to fetch trailers');
+      }
+    } catch (e) {
+      throw Exception('Error fetching trailers: $e');
+    }
+  }
+
+  Future<List<MediaContentModel>> fetchSimilarMovies(int id) async {
+    try {
+      final Response<dynamic> response = await apiClient.get('movie/$id/similar?api_key=$apiKey');
+      if (response.statusCode == 200 && response.data != null) {
+        List<dynamic> similarMoviesJson = response.data!['results'];
+        return similarMoviesJson.map((dynamic item) => MediaContentModel.fromJson(item)).toList();
+      } else {
+        throw Exception('Failed to fetch similar movies data');
+      }
+    } catch (e) {
+      throw Exception('Error fetching similar movies data: $e');
+    }
+  }
+
+  Future<List<MediaContentModel>> fetchRecommendedMovies(int id) async {
+    try {
+      final Response<dynamic> response = await apiClient.get('movie/$id/recommendations?api_key=$apiKey');
+      if (response.statusCode == 200 && response.data != null) {
+        List<dynamic> recommendedMoviesJson = response.data!['results'];
+        return recommendedMoviesJson.map((dynamic item) => MediaContentModel.fromJson(item)).toList();
+      } else {
+        throw Exception('Failed to fetch recommended movies data');
+      }
+    } catch (e) {
+      throw Exception('Error fetching recommended movies data: $e');
+    }
+  }
+
+  Future<MediaContentDetailsModel> fetchMovieDetails(int id) async {
+    try {
+      final Response<dynamic> response = await apiClient.get('movie/$id?api_key=$apiKey');
+      if (response.statusCode == 200 && response.data != null) {
+        return MediaContentDetailsModel.fromJson(response.data);
+      } else {
+        throw Exception('Failed to fetch movie details data');
+      }
+    } catch (e) {
+      throw Exception('Error fetching movie details data: $e');
+    }
+  }
+
+  Future<List<Review>> fetchUserReviews(int id) async {
+    try {
+      final Response<dynamic> response = await apiClient.get('movie/$id/reviews?api_key=$apiKey');
+      if (response.statusCode == 200 && response.data != null) {
+        List<dynamic> userReviewsJson = response.data!['results'];
+        return userReviewsJson.map((dynamic item) => Review.fromJson(item)).toList();
+      } else {
+        throw Exception('Failed to fetch movie user reviews data');
+      }
+    } catch (e) {
+      throw Exception('Error fetching movie user reviews data: $e');
+    }
+  }
+}
