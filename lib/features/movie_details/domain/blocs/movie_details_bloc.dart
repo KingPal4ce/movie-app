20c6eab0ca1a8f6e02adcfa6792f56bca13e4e9c
Ricketson Palacio <i_ricketson.palacio@stratpoint.com>
2025-03-25 13:28:13 +0800

    feat(bloc): Implemented bloc for movie details

---
+++ lib/features/movie_details/domain/blocs/movie_details_bloc.dart
@@ -0,0 +1,123 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_details_model.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_model.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_reviews_model.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/trailers_model.dart';
+import 'package:flutter_intro_bootcamp_project/features/movie_details/domain/blocs/movie_details_states.dart';
+import 'package:flutter_intro_bootcamp_project/features/movie_details/domain/repositories/movie_details_repo.dart';
+
+class MovieDetailsBloc extends Cubit<MovieDetailsStates> {
+  MovieDetailsBloc({required this.movieDetailsRepo}) : super(MovieDetailsInitial());
+
+  final MovieDetailsRepo movieDetailsRepo;
+  MediaContentDetailsModel? _movieDetails;
+  TrailersModel? _trailers;
+  List<MediaContentModel>? _similarMovies;
+  List<MediaContentModel>? _recommendedMovies;
+  List<Map<String, dynamic>>? _reviews;
+
+  Future<void> fetchTrailers(int movieId) async {
+    try {
+      emit(MovieDetailsLoading());
+      _trailers = await movieDetailsRepo.fetchTrailers(movieId);
+      emit(
+        MovieDetailsLoaded(
+          movieDetails: _movieDetails,
+          trailers: _trailers,
+          similarMovies: _similarMovies,
+          recommendedMovies: _recommendedMovies,
+          reviews: _reviews,
+        ),
+      );
+    } catch (e) {
+      emit(MovieDetailsError('$e'));
+    }
+  }
+
+  Future<void> fetchSimilarMovies(int movieId) async {
+    try {
+      emit(MovieDetailsLoading());
+      _similarMovies = await movieDetailsRepo.fetchSimilarMovies(movieId);
+      emit(
+        MovieDetailsLoaded(
+          movieDetails: _movieDetails,
+          trailers: _trailers,
+          similarMovies: _similarMovies,
+          recommendedMovies: _recommendedMovies,
+          reviews: _reviews,
+        ),
+      );
+    } catch (e) {
+      emit(MovieDetailsError('$e'));
+    }
+  }
+
+  Future<void> fetchRecommendedMovies(int movieId) async {
+    try {
+      emit(MovieDetailsLoading());
+      _recommendedMovies = await movieDetailsRepo.fetchRecommendedMovies(movieId);
+      emit(
+        MovieDetailsLoaded(
+          movieDetails: _movieDetails,
+          trailers: _trailers,
+          similarMovies: _similarMovies,
+          recommendedMovies: _recommendedMovies,
+          reviews: _reviews,
+        ),
+      );
+    } catch (e) {
+      emit(MovieDetailsError('$e'));
+    }
+  }
+
+  Future<void> fetchMovieDetails(int movieId) async {
+    try {
+      emit(MovieDetailsLoading());
+      _movieDetails = await movieDetailsRepo.fetchMovieDetails(movieId);
+      emit(
+        MovieDetailsLoaded(
+          movieDetails: _movieDetails,
+          trailers: _trailers,
+          similarMovies: _similarMovies,
+          recommendedMovies: _recommendedMovies,
+          reviews: _reviews,
+        ),
+      );
+    } catch (e) {
+      emit(MovieDetailsError('$e'));
+    }
+  }
+
+  Future<void> fetchUserReviews(int movieId) async {
+    try {
+      emit(MovieDetailsLoading());
+      final List<Review> reviewsList = await movieDetailsRepo.fetchUserReviews(movieId);
+
+      _reviews =
+          reviewsList
+              .map(
+                (Review review) => <String, String>{
+                  "name": review.author,
+                  "review": review.content,
+                  "rating": review.authorDetails.displayRating,
+                  "avatarPhoto": review.authorDetails.displayAvatar,
+                  "creationDate": review.createdAt.substring(0, 10),
+                  "fullReviewUrl": review.url,
+                },
+              )
+              .toList();
+
+      emit(
+        MovieDetailsLoaded(
+          movieDetails: _movieDetails,
+          trailers: _trailers,
+          similarMovies: _similarMovies,
+          recommendedMovies: _recommendedMovies,
+          reviews: _reviews,
+        ),
+      );
+    } catch (e) {
+      emit(MovieDetailsError('Failed to fetch reviews: $e'));
+    }
+  }
+}
---
+++ lib/features/movie_details/domain/blocs/movie_details_states.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_details_model.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_model.dart';
+import 'package:flutter_intro_bootcamp_project/core/data/models/trailers_model.dart';
+
+abstract class MovieDetailsStates {}
+
+// initial
+class MovieDetailsInitial extends MovieDetailsStates {}
+
+// loading
+class MovieDetailsLoading extends MovieDetailsStates {}
+
+// loaded
+class MovieDetailsLoaded extends MovieDetailsStates {
+  MovieDetailsLoaded({this.similarMovies, this.recommendedMovies, this.reviews, this.trailers, this.movieDetails});
+  final List<MediaContentModel>? similarMovies;
+  final List<MediaContentModel>? recommendedMovies;
+  final List<Map<String, dynamic>>? reviews;
+  final TrailersModel? trailers;
+  final MediaContentDetailsModel? movieDetails;
+}
+
+// errors
+class MovieDetailsError extends MovieDetailsStates {
+  MovieDetailsError(this.message);
+  final String message;
+}
