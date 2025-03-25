9646208fba57ae78b123241c02d4d154bf02f8cd
Ricketson Palacio <i_ricketson.palacio@stratpoint.com>
2025-03-25 11:26:50 +0800

    feat(navigation): Added a navigation with screen with the google nav bar for showing different screens

---
+++ lib/core/presentation/screens/navigation_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_intro_bootcamp_project/features/home/presentation/screens/home_screen.dart';
+import 'package:flutter_intro_bootcamp_project/features/movie/presentation/screens/movies_screen.dart';
+import 'package:flutter_intro_bootcamp_project/features/search/presentation/screens/search_screen.dart';
+import 'package:flutter_intro_bootcamp_project/features/settings/presentation/screens/settings_screen.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({super.key});
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  int _selectedIndex = 0;
+
+  void _navigateBottomBar(int index) {
+    setState(() => _selectedIndex = index);
+  }
+
+  final List<Widget> _screens = <Widget>[HomeScreen(), MoviesScreen(), SearchScreen(), SettingsScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: Container(
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+            child: GNav(
+              color: Colors.white,
+              activeColor: Colors.white,
+              tabBackgroundColor: Colors.grey.shade800,
+              gap: 8,
+              padding: EdgeInsets.all(16),
+              onTabChange: _navigateBottomBar,
+              tabs: const <GButton>[
+                GButton(icon: Icons.home, text: 'Home'),
+                GButton(icon: Icons.movie, text: 'Movies'),
+                GButton(icon: Icons.search, text: 'Search'),
+                GButton(icon: Icons.settings, text: 'Settings'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
