0f474da54d8dfa1d6b34615c92d7d5225c17d66a
Ricketson Palacio <i_ricketson.palacio@stratpoint.com>
2025-03-25 11:24:28 +0800

    feat(data): added trailers model for json parsing

---
+++ lib/core/data/models/trailers_model.g.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'trailers_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TrailersModel _$TrailersModelFromJson(Map<String, dynamic> json) =>
+    TrailersModel(key: json['key'] as String, site: json['site'] as String, type: json['type'] as String);
+
+Map<String, dynamic> _$TrailersModelToJson(TrailersModel instance) => <String, dynamic>{
+  'key': instance.key,
+  'site': instance.site,
+  'type': instance.type,
+};
