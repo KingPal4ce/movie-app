47599cc6521efa5d70f98cda66d7acb34a175e4a
Ricketson Palacio <ricketson.king@gmail.com>
2025-07-23 20:09:37 +0800

    refactor(AuthState): change AuthState to AuthStates

--- lib/features/auth/domain/blocs/auth_states.dart
+++ lib/features/auth/domain/blocs/auth_states.dart
@@ -1,24 +1,24 @@
 import 'package:flutter_intro_bootcamp_project/features/auth/data/models/app_user.dart';
 
-abstract class AuthState {}
+abstract class AuthStates {}
 
 // initial
-class AuthInitial extends AuthState {}
+class AuthInitial extends AuthStates {}
 
 // loading
-class AuthLoading extends AuthState {}
+class AuthLoading extends AuthStates {}
 
 // authenticated
-class Authenticated extends AuthState {
+class Authenticated extends AuthStates {
   Authenticated(this.user);
   final AppUser? user;
 }
 
 // unauthenticated
-class Unauthenticated extends AuthState {}
+class Unauthenticated extends AuthStates {}
 
 // errors
-class AuthError extends AuthState {
+class AuthError extends AuthStates {
   AuthError(this.message);
   final String message;
 }
