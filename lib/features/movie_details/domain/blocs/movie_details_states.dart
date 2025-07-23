aab1f0f080a00f5537bc44632e72f9eb6ad2b362
Ricketson Palacio <ricketson.king@gmail.com>
2025-07-23 20:42:29 +0800

    feat(MovieDetailsStates): add castMembers

--- lib/features/movie_details/domain/blocs/movie_details_states.dart
+++ lib/features/movie_details/domain/blocs/movie_details_states.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_model.dart';
-import 'package:flutter_intro_bootcamp_project/core/data/models/trailers_model.dart';
+import 'package:flutter_intro_bootcamp_project/features/movie_details/data/models/cast_member_model.dart';
 import 'package:flutter_intro_bootcamp_project/features/movie_details/data/models/media_content_details_model.dart';
+import 'package:flutter_intro_bootcamp_project/features/movie_details/data/models/trailers_model.dart';
 
 abstract class MovieDetailsStates {}
 
@@ -12,12 +13,15 @@ class MovieDetailsLoading extends MovieDetailsStates {}
 
 // loaded
 class MovieDetailsLoaded extends MovieDetailsStates {
-  MovieDetailsLoaded({this.similarMovies, this.recommendedMovies, this.reviews, this.trailers, this.movieDetails});
+  MovieDetailsLoaded({this.similarMovies, this.recommendedMovies, this.reviews, this.trailers, this.movieDetails, this.castMembers});
   final List<MediaContentModel>? similarMovies;
   final List<MediaContentModel>? recommendedMovies;
   final List<Map<String, dynamic>>? reviews;
+  final List<CastMemberModel>? castMembers;
   final TrailersModel? trailers;
   final MediaContentDetailsModel? movieDetails;
+
+  get cast => null;
 }
 
 // errors
