0124cc1cb7115f20f0aca4bcd6899527145de9e8
Ricketson Palacio <ricketson.king@gmail.com>
2025-07-23 20:42:52 +0800

    feat(MovieDetailsRepo): add fetchCastMember

--- lib/features/movie_details/domain/repositories/movie_details_repo.dart
+++ lib/features/movie_details/domain/repositories/movie_details_repo.dart
@@ -1,7 +1,8 @@
 import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_model.dart';
-import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_reviews_model.dart';
-import 'package:flutter_intro_bootcamp_project/core/data/models/trailers_model.dart';
+import 'package:flutter_intro_bootcamp_project/features/movie_details/data/models/cast_member_model.dart';
 import 'package:flutter_intro_bootcamp_project/features/movie_details/data/models/media_content_details_model.dart';
+import 'package:flutter_intro_bootcamp_project/features/movie_details/data/models/media_content_reviews_model.dart';
+import 'package:flutter_intro_bootcamp_project/features/movie_details/data/models/trailers_model.dart';
 
 abstract class MovieDetailsRepo {
   Future<TrailersModel?> fetchTrailers(int movieId);
@@ -13,4 +14,6 @@ abstract class MovieDetailsRepo {
   Future<MediaContentDetailsModel> fetchMovieDetails(int id);
 
   Future<List<Review>> fetchUserReviews(int id);
+
+  Future<List<CastMemberModel>> fetchCastMember(int id);
 }
