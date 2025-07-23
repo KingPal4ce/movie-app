d1ac25114060be2cef2d402dd534b7cf5b4de2fd
Ricketson Palacio <ricketson.king@gmail.com>
2025-07-23 20:15:32 +0800

    refactor(HomeBloc): fetch movies as a whole

--- lib/features/home/domain/blocs/home_bloc.dart
+++ lib/features/home/domain/blocs/home_bloc.dart
@@ -1,60 +1,37 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_model.dart';
-import 'package:flutter_intro_bootcamp_project/features/home/domain/blocs/home_state.dart';
+import 'package:flutter_intro_bootcamp_project/features/home/domain/blocs/home_states.dart';
 import 'package:flutter_intro_bootcamp_project/features/home/domain/repositories/home_repo.dart';
 
-class HomeBloc extends Cubit<HomeState> {
+class HomeBloc extends Cubit<HomeStates> {
   HomeBloc({required this.homeRepo}) : super(HomeInitial());
 
   final HomeRepo homeRepo;
+  late List<MediaContentModel> nowPlayingMovies;
+  late List<MediaContentModel> topRatedMovies;
+  late List<MediaContentModel> popularMovies;
+  late List<MediaContentModel> trendingDay;
+  late List<MediaContentModel> trendingWeek;
 
-  Future<List<MediaContentModel>> fetchTrendingWeek() async {
+  Future<void> fetchMovies() async {
     try {
       emit(HomeLoading());
-      return await homeRepo.fetchTrendingWeek();
+      trendingWeek = await homeRepo.fetchTrendingWeek();
+      trendingDay = await homeRepo.fetchTrendingDay();
+      popularMovies = await homeRepo.fetchPopularMovies();
+      topRatedMovies = await homeRepo.fetchTopRatedMovies();
+      nowPlayingMovies = await homeRepo.fetchNowPlayingMovies();
+      emit(
+        HomeLoaded(
+          trendingWeek: trendingWeek,
+          trendingDay: trendingDay,
+          popularMovies: popularMovies,
+          topRatedMovies: topRatedMovies,
+          nowPlayingMovies: nowPlayingMovies,
+        ),
+      );
     } catch (e) {
-      emit(HomeError('Failed to fetch trending week: $e'));
-      throw Exception('Error in HomeBloc (fetchTrendingWeek): $e');
-    }
-  }
-
-  Future<List<MediaContentModel>> fetchTrendingDay() async {
-    try {
-      emit(HomeLoading());
-      return await homeRepo.fetchTrendingDay();
-    } catch (e) {
-      emit(HomeError('Failed to fetch trending day: $e'));
-      throw Exception('Error in HomeBloc (fetchTrendingDay): $e');
-    }
-  }
-
-  Future<List<MediaContentModel>> fetchPopularMovies() async {
-    try {
-      emit(HomeLoading());
-      return await homeRepo.fetchPopularMovies();
-    } catch (e) {
-      emit(HomeError('Failed to fetch popular movies: $e'));
-      throw Exception('Error in HomeBloc (fetchPopularMovies): $e');
-    }
-  }
-
-  Future<List<MediaContentModel>> fetchTopRatedMovies() async {
-    try {
-      emit(HomeLoading());
-      return await homeRepo.fetchTopRatedMovies();
-    } catch (e) {
-      emit(HomeError('Failed to fetch top rated movies: $e'));
-      throw Exception('Error in HomeBloc (fetchTopRatedMovies): $e');
-    }
-  }
-
-  Future<List<MediaContentModel>> fetchNowPlayingMovies() async {
-    try {
-      emit(HomeLoading());
-      return await homeRepo.fetchNowPlayingMovies();
-    } catch (e) {
-      emit(HomeError('Failed to fetch now playing movies: $e'));
-      throw Exception('Error in HomeBloc (fetchNowPlayingMovies): $e');
+      emit(HomeError('Failed to fetch movies: $e'));
     }
   }
 }
