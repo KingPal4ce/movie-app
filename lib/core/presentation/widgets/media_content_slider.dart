719cd5d3c3030ef93468b973321eede79996039d
Ricketson Palacio <ricketson.king@gmail.com>
2025-07-23 20:06:05 +0800

    feat(MediaContentSlider): add placeholder for movies without posters

--- lib/core/presentation/widgets/media_content_slider.dart
+++ lib/core/presentation/widgets/media_content_slider.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_model.dart';
 import 'package:flutter_intro_bootcamp_project/features/movie_details/presentation/screens/movie_details_screen.dart';
@@ -15,7 +16,7 @@ class MediaContentSlider extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Padding(padding: EdgeInsets.only(left: 10, top: 25, bottom: 35), child: Text(categoryTitle, style: GoogleFonts.poppins(fontSize: 18))),
+        Padding(padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20), child: Text(categoryTitle, style: GoogleFonts.poppins(fontSize: 18))),
         SizedBox(
           height: 250,
           child: ListView.builder(
@@ -29,37 +30,55 @@ class MediaContentSlider extends StatelessWidget {
                   Navigator.push(context, MaterialPageRoute<Widget>(builder: (BuildContext context) => MovieDetailsScreen(movieId: mediaContent.id)));
                 },
                 child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    image: DecorationImage(
-                      colorFilter: ColorFilter.mode(Colors.black.withValues(alpha: 0.3), BlendMode.darken),
-                      image: NetworkImage('https://image.tmdb.org/t/p/w500${mediaContent.posterPath}'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
+                  decoration: BoxDecoration(color: Theme.of(context).colorScheme.secondary, borderRadius: BorderRadius.circular(10)),
                   margin: EdgeInsets.only(left: 13),
-                  width: 170,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(top: 2, right: 6),
-                        child: Container(
-                          decoration: BoxDecoration(color: Colors.black.withValues(alpha: 0.5), borderRadius: BorderRadius.circular(5)),
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
-                            child: Row(
-                              children: <Widget>[
-                                const Icon(Icons.star, color: Colors.yellow, size: 15),
-                                SizedBox(width: 2),
-                                Text(mediaContent.voteAverage.toString()),
-                              ],
-                            ),
+                  child: Center(
+                    child: Stack(
+                      children: <Widget>[
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: CachedNetworkImage(
+                            imageUrl: mediaContent.fullPosterPath ?? '',
+                            placeholder:
+                                (BuildContext context, String url) =>
+                                    Padding(padding: const EdgeInsets.all(50), child: Center(child: CircularProgressIndicator())),
+                            errorWidget:
+                                (BuildContext context, String url, Object error) => Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                                  child: Center(
+                                    child: Image.asset(
+                                      'assets/images/placeholder/placeholder-image.png',
+                                      color: Theme.of(context).colorScheme.primary,
+                                      width: 110,
+                                    ),
+                                  ),
+                                ),
+                            fit: BoxFit.fill,
                           ),
                         ),
-                      ),
-                    ],
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(top: 3, left: 3),
+                              child: Container(
+                                decoration: BoxDecoration(color: Colors.black.withValues(alpha: 0.5), borderRadius: BorderRadius.circular(5)),
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
+                                  child: Row(
+                                    children: <Widget>[
+                                      const Icon(Icons.star, color: Colors.yellow, size: 15),
+                                      SizedBox(width: 2),
+                                      Text(mediaContent.voteAverage!.toStringAsFixed(1)),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
