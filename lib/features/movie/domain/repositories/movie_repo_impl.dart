621169f18e2b6cd6f0ad7b3dd008e6b9805492d3
Ricketson Palacio <ricketson.king@gmail.com>
2025-07-23 20:37:22 +0800

    refactor(MovieRepoImpl): remove unnecessary fetches

--- lib/features/movie/domain/repositories/movie_repo_impl.dart
+++ lib/features/movie/domain/repositories/movie_repo_impl.dart
@@ -1,9 +1,6 @@
 import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_model.dart';
-import 'package:flutter_intro_bootcamp_project/core/data/models/media_content_reviews_model.dart';
-import 'package:flutter_intro_bootcamp_project/core/data/models/trailers_model.dart';
 import 'package:flutter_intro_bootcamp_project/features/movie/data/services/movie_service.dart';
 import 'package:flutter_intro_bootcamp_project/features/movie/domain/repositories/movie_repo.dart';
-import 'package:flutter_intro_bootcamp_project/features/movie_details/data/models/media_content_details_model.dart';
 
 class MovieRepoImpl implements MovieRepo {
   MovieRepoImpl(this.movieService);
@@ -15,51 +12,6 @@ class MovieRepoImpl implements MovieRepo {
       return await movieService.fetchMoviesByGenre(genre);
     } catch (e) {
       throw Exception('Error in MovieRepoImpl (fetchMoviesByGenre): $e');
-    }
-  }
-
-  @override
-  Future<List<MediaContentModel>> fetchRecommendedMovies(int id) async {
-    try {
-      return await movieService.fetchRecommendedMovies(id);
-    } catch (e) {
-      throw Exception('Error in MovieRepoImpl (fetchRecommendedMovies): $e');
-    }
-  }
-
-  @override
-  Future<List<MediaContentModel>> fetchSimilarMovies(int id) async {
-    try {
-      return await movieService.fetchSimilarMovies(id);
-    } catch (e) {
-      throw Exception('Error in MovieRepoImpl (fetchSimilarMovies): $e');
-    }
-  }
-
-  @override
-  Future<TrailersModel?> fetchTrailers(int movieId) async {
-    try {
-      return await movieService.fetchTrailers(movieId);
-    } catch (e) {
-      throw Exception('Error in MovieRepoImpl (fetchTrailers): $e');
-    }
-  }
-
-  @override
-  Future<List<Review>> fetchUserReviews(int id) async {
-    try {
-      return await movieService.fetchUserReviews(id);
-    } catch (e) {
-      throw Exception('Error in MovieRepoImpl (fetchUserReviews): $e');
-    }
-  }
-
-  @override
-  Future<MediaContentDetailsModel> fetchMovieDetails(int id) async {
-    try {
-      return await movieService.fetchMovieDetails(id);
-    } catch (e) {
-      throw Exception('Error in MovieRepoImpl (fetchMovieDetails): $e');
     }
   }
 }
